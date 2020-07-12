929d76c02b555d6e2d4222ae25a65ddd65f534a5
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-07-12 23:23:26 +0700

    todo

---
+++ angular-http/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from "@angular/common/http";
+import { TodoComponent } from './todo/todo.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {TokenInterceptor} from "./token.interceptor";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptor,
+    multi: true
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
